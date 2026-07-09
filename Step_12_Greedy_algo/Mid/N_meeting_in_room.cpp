cc26f404fde5f474cc06fbf73f632fd8cc147904
coderninjai <pradipgajghate1@gmail.com>
2026-07-10 00:49:33 +0530

    N meetings in a room

---
+++ Step_12_Greedy_algo/Mid/N_meeting_in_room.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Class to hold solution logic
+class Solution {
+public:
+    // Function to get all meetings that can be scheduled
+    vector<int> maxMeetings(vector<int>& start, vector<int>& end) {
+        // Store meetings as (end_time, start_time, original_index)
+        vector<tuple<int, int, int>> meetings;
+        for (int i = 0; i < start.size(); i++) {
+            // i+1 for 1-based indexing
+            meetings.push_back({end[i], start[i], i + 1}); 
+           
+        }
+
+        // Sort by end time
+        sort(meetings.begin(), meetings.end());
+
+        vector<int> result; // To store meeting indices
+        int lastEnd = -1;
+
+        // Traverse sorted meetings
+        for (auto& m : meetings) {
+            int e = get<0>(m);
+            int s = get<1>(m);
+            int idx = get<2>(m);
+
+            // If meeting starts after last one ends
+            if (s > lastEnd) {
+                // Store index
+                result.push_back(idx); 
+                // Update last end time
+                lastEnd = e; 
+            }
+        }
+        return result;
+    }
+};
+
+// Main driver code
+int main() {
+    vector<int> start = {1, 3, 0, 5, 8, 5};
+    vector<int> end   = {2, 4, 6, 7, 9, 9};
+
+    Solution sol;
+    vector<int> res = sol.maxMeetings(start, end);
+
+    for (int idx : res) cout << idx << " ";
+}
+    
